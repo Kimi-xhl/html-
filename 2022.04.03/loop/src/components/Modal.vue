55bc675b5178d95bd09804edd3ee8b83f7e9074a
XuHongli <egoistxu@foxmail.com>
2022-04-03 18:24:29 +0800

    aaa

---
+++ 2022.04.03/loop/src/components/Modal.vue
@@ -0,0 +1,56 @@
+<template>
+  <div v-if="display">
+    <div class="cover"  @click="clickCover()"></div>
+    <div class="Modal" style="width: 400px; height: 200px">
+      <div class="header">
+        <p>{{ title  }}</p>
+      </div>
+      <div class="main"></div>
+      <div class="footer"></div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    title: {
+      type: String,
+      default: 'this is title', // 默认值
+    },
+    display: {
+      type: Boolean,
+      required: true // 是否必填
+    }
+  },
+  created() {
+    console.log(this.display)
+  },
+  methods: {
+    clickCover() {
+      this.$parent.modalShow = false
+    }
+  },
+}
+</script>
+
+<style scoped>
+.Modal{
+  background-color: #fcfbfb;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  border-radius: 5px;
+}
+.cover{
+  background-color: rgba(0,0,0,.5);
+  width: 100vw;
+  height: 100vh;
+  top: 0;
+  left: 0;
+  position: absolute;
+}
+</style>
---
+++ 2022.04.03/loop/src/views/HomeView.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" @click="showAlert">
+    <p :class="{title:false}">{{ p | before }}</p>
+    <p>{{ p1  }}</p>
+    <p>{{ name }}</p>
+    <Carsrd></Carsrd>
+    <Modal title="ddd" :display="modalShow" ></Modal>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Modal from "@/components/Modal";
+import Carsrd from "@/components/Carsrd";
+export default {
+  name: 'HomeView',
+  components: {
+    Carsrd,
+    Modal
+  },
+  computed: {
+    name() {
+      return this.p + this.p1
+    }
+  },
+  filters: {
+    before: function (value) {
+      return 'this is ' + value;
+    }
+  },
+  data() {
+    return {
+      p: 'sdsddd',
+      p1: '111',
+      modalShow: false
+    }
+  },
+  methods: {
+    showAlert() {
+      this.modalShow = !this.modalShow
+      this.p = '1563615'
+    }
+  }
+}
+</script>
